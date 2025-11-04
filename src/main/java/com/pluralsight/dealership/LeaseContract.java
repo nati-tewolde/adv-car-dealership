087377472e948031c6a2f8ee7ebfb863572b4f6f
nati-tewolde <natisnewemail@gmail.com>
2025-11-04 16:09:03 -0500

    (Phase 1) Add LeaseContract subclass - Add attributes, constructor and getters/setters - Override getTotalPrice() and getMonthlyPayment()

---
+++ src/main/java/com/pluralsight/dealership/LeaseContract.java
@@ -0,0 +1,44 @@
+package com.pluralsight.dealership;
+
+public class LeaseContract extends Contract {
+    private double expectedEndingValue;
+    private double leaseFee;
+
+    public LeaseContract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold) {
+        super(contractDate, customerName, customerEmail, vehicleSold);
+        this.expectedEndingValue = vehicleSold.getPrice() * 0.5;
+        this.leaseFee = vehicleSold.getPrice() * 0.07;
+    }
+
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public void setExpectedEndingValue(double expectedEndingValue) {
+        this.expectedEndingValue = expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    public void setLeaseFee(double leaseFee) {
+        this.leaseFee = leaseFee;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return (getVehicleSold().getPrice() - expectedEndingValue) + leaseFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 36;
+        double interestRate = 4.0 / 1200;
+        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+        monthlyPayment = Math.round(monthlyPayment * 100);
+        monthlyPayment /= 100;
+        return monthlyPayment;
+    }
+
+}
