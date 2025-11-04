21528c94851f8448b7933d4db4df767b77c14c4f
nati-tewolde <natisnewemail@gmail.com>
2025-11-04 15:23:34 -0500

    (Phase 1) Add Contract abstract class - Add attributes, constructor and getters/setters - Define getTotalPrice() and getMonthlyPayment() abstract methods

---
+++ src/main/java/com/pluralsight/dealership/Contract.java
@@ -0,0 +1,54 @@
+package com.pluralsight.dealership;
+
+public abstract class Contract {
+    private String contractDate;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+    // private double totalPrice;
+    // private double monthlyPayment;
+
+    public Contract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.contractDate = contractDate;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    public String getContractDate() {
+        return contractDate;
+    }
+
+    public void setContractDate(String contractDate) {
+        this.contractDate = contractDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotalPrice();
+
+    public abstract double getMonthlyPayment();
+
+}
